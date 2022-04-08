470bad949780e8b7d98ee0c53f35b204b273dbca
Valeria Vitvytska <leranmu@gmail.com>
2022-04-08 09:41:12 +0300

    commit4

---
+++ 4.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+let num1 = +prompt('Enter fitst number');
+
+let num2 = +prompt('Enter second number');
+
+
+while (num1 === 0 || isNaN(num1) || num2 === 0 || isNaN(num2)) {
+
+  num1 = +prompt('Enter first number', num1);
+
+  num2 = +prompt('Enter second number', num2);
+
+}
+
+
+let operator = prompt('Введите операцию с числами: + или  - или * или /');
+
+
+while (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
+
+  operator = prompt('Введите операцию с числами: + или  - или * или /');
+
+}
+
+let calcResult = () => {
+
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+
+  }
+}
+
+console.log(calcResult());
+
+
