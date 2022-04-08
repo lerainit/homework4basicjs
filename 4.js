cfe3c5e18e21babf746c02bb262e046c4281c9f6
Valeria Vitvytska <leranmu@gmail.com>
2022-04-08 14:14:52 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -8,9 +8,9 @@ let num2 = +prompt('Enter second number');
 
 while (num1 === 0 || isNaN(num1) || num2 === 0 || isNaN(num2)) {
 
-  num1 = +prompt('Enter first number', num1);
+  num1 = +prompt('Введите первое число', num1);
 
-  num2 = +prompt('Enter second number', num2);
+  num2 = +prompt('Введите второе число', num2);
 
 }
 
